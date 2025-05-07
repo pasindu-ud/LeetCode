62756541bf3a3c10989ea01cf3175a798fc92e8b
Pasindu Udawatta <61894712+pasindu-ud@users.noreply.github.com>
2025-05-08 09:20:13 +1200

    Add time and space complexity

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,11 +1,11 @@
 # One-pass approach
-# Time complexity = O(n)
-# Space complexity = O(n)
 # Exactly one and only solution exists
 # nums = [.., x, .., y, ..]
 # x + y = target => x = target - y
 # Maintain a record of all previously accessed values in the list
 # @ y, check whether x exists in the record
+# Time complexity = O(n)
+# Space complexity = O(n)
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
